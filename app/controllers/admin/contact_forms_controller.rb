dc72b4cdb6964d086e177c3cfcfa67ff8656f57c
Jury Ghidinelli <jury@archimedianet.it>
2018-06-08 17:26:37 +0200

    fixed #6

--- app/controllers/admin/contact_forms_controller.rb
+++ app/controllers/admin/contact_forms_controller.rb
@@ -1,5 +1,8 @@
 module Admin
   class ContactFormsController < UserSiteRegistrationsController
 
+    def resource_handler
+      @_resource_handler ||= ::ContactFormResource.new(controller_path, alchemy_module)
+    end
   end
 end
