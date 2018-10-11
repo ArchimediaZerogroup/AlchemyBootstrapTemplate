d5ac1829d7cb692ab131ac48176be327f6b035f5
Jury Ghidinelli <jury@archimedianet.it>
2018-10-11 17:48:54 +0200

    recaptcha in sviluppo

--- template.rb
+++ template.rb
@@ -159,8 +159,11 @@ end
 Recaptcha.configure do |config|
   config.site_key  = Rails.application.secrets.recaptcha.nil? ? "" : Rails.application.secrets.recaptcha[:site_key]
   config.secret_key = Rails.application.secrets.recaptcha.nil? ? "" : Rails.application.secrets.recaptcha[:secret_key]
+  config.skip_verify_env += ["development"]
 end
     CODE
+
+
 
     say "Remember! You must complete configuration into initializer config/initializers/recaptcha.rb with API-KEY ", [:red, :on_white, :bold]
 
