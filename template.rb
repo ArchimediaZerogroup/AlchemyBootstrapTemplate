a725cb1f0e8c1adce037bba9cc0f391a1fd4b1ed
Jury Ghidinelli <juryghidinelli@gmail.com>
2020-03-22 13:55:24 +0100

    wip fix errors

--- template.rb
+++ template.rb
@@ -1,78 +1,21 @@
 require "open-uri"
 require 'yaml'
 
-version = %x(bin/rails version).gsub("\n", "").gsub("Rails", "")
-gem_version = Gem::Version.new(version)
-REPOSITORY_URL = "https://github.com/ArchimediaZerogroup/AlchemyBootstrapTemplate/raw/master"
 
 
-ask("Remeber!!!! DISABLE_SPRING=true before command.")
-say "You are using Rails #{gem_version.inspect}"
-if gem_version <= Gem::Version.new("5.2.4")
+
+
+
+def default_patch(sass_requires,application_js, application_css)
   
-  default_gems
-  default_patch
-  bootstrap4
-  fontawesome
-  cookie_law
-  recaptcha_gem
-  redis_backend
-
-  after_bundle do
-    rails_command 'alchemy:install'
-    generate 'friendly_id'
-    generate 'cookie_law:install'
-    rails_command 'alchemy_custom_model:install'
-    download_file "config/locales/devise.it.yml"
-    download_file "config/puma.rb"    
-
-    if pg_search
-      pg_search_finalize
-    end
-
-    capistrano_finalize
-    bootstrap_template
-
-    download_file "app/helpers/link_languages_helper_decorator.rb"
-    say "Created 'language_links_by_page' helper that must be insert into layouts (<%= language_links_by_page(@page)  %>)", [:red, :on_white, :bold]
-
-    generate 'alchemy_i18n:install --locales=it'
-
-    #Configure Alchemy defaults
-    append_to_file "config/alchemy/config.yml" do
-      "\nitems_per_page: 100"
-    end
-    #Cache assets initializer
-    download_file "config/initializers/static_assets_cache.rb"
-    # Add devise css require
-    generate 'alchemy:devise:install'
-    rails_command "alchemy_custom_model:install"
-    
-    ["lib/tasks/alchemy_cache_clear.rake"].each do |f|
-      download_file f
-    end
-
-  end  
-
-else
-  raise "Alchemy it's not compatible with Rails version"
-end
-
-
-
-
-def default_patch
-  application_js = 'app/assets/javascripts/application.js'
   inject_into_file application_js, before: '//= require_tree .' do
     "\n//= require jquery3\n//= require jquery_ujs\n"
   end
-
-  application_css = 'app/assets/stylesheets/application.css'
+  
   inject_into_file application_css, :before => " */" do
     "\n  *= require sass_requires\n"
   end
-
-  sass_requires = 'app/assets/stylesheets/sass_requires.scss'
+  
 
   file sass_requires, <<-CODE
 //
@@ -98,7 +41,7 @@ CODE
 
 end
 
-def bootstrap4
+def bootstrap4(sass_requires,application_js)
   gem 'bootstrap', '~> 4.1', '>= 4.1.3'
   inject_into_file sass_requires, before: '//= require_tree .' do
     "\n  @import \"bootstrap\";\n"
@@ -108,7 +51,7 @@ def bootstrap4
   end
 end
 
-def fontawesome
+def fontawesome(application_css)
   gem "font-awesome-rails"
 
   inject_into_file application_css, :before => " */" do
@@ -116,7 +59,7 @@ def fontawesome
   end
 end
 
-def cookie_law
+def cookie_law(application_css, application_js)
   gem "cookie_law"
 
   inject_into_file application_css, :before => " */" do
@@ -226,33 +169,33 @@ def default_gems(deploy_with_docker=true)
 
 end
 
-def download_file(source_path, destination: nil, repository_url: REPOSITORY_URL)
+def download_file(source_path, destination=nil, repository_url="")
   destination = destination || source_path
   get "#{repository_url}/#{source_path}", destination
 end
 
-def pg_search_finalize
+def pg_search_finalize(repository_url)
   generate 'pg_search:migration:multisearch'
-  download_file "app/lib/search_result.rb"
-  download_file "app/models/alchemy/content_decorator.rb"
-  download_file "app/models/alchemy/essence_html_decorator.rb"
-  download_file "app/models/alchemy/essence_richtext_decorator.rb"
-  download_file "app/models/alchemy/essence_text_decorator.rb"
-  download_file "app/models/alchemy/page_decorator.rb"
-  download_file "app/models/concerns/alchemy/content_dec.rb"
-  download_file "app/models/concerns/alchemy/essence_html_dec.rb"
-  download_file "app/models/concerns/alchemy/essence_richtext_dec.rb"
-  download_file "app/models/concerns/alchemy/essence_text_dec.rb"
-  download_file "app/models/concerns/alchemy/page_dec.rb"
-  download_file "app/models/concerns/searchable.rb"
-  download_file "app/views/alchemy/search/_form.html.erb"
-  download_file "app/views/alchemy/search/_result.html.erb"
-  download_file "app/views/alchemy/search/_results.html.erb"
-  download_file "db/migrate/20180405200556_add_searchable_to_alchemy_essence_texts.alchemy_pg_search.rb"
-  download_file "db/migrate/20180405200557_add_searchable_to_alchemy_essence_richtexts.alchemy_pg_search.rb"
-  download_file "db/migrate/20180405200558_add_searchable_to_alchemy_essence_pictures.alchemy_pg_search.rb"
-  download_file "config/initializers/archimedia_pgsearch.rb"
-  download_file "config/initializers/pg_search.rb"
+  download_file "app/lib/search_result.rb",nil,repository_url
+  download_file "app/models/alchemy/content_decorator.rb",nil,repository_url
+  download_file "app/models/alchemy/essence_html_decorator.rb",nil,repository_url
+  download_file "app/models/alchemy/essence_richtext_decorator.rb",nil,repository_url
+  download_file "app/models/alchemy/essence_text_decorator.rb",nil,repository_url
+  download_file "app/models/alchemy/page_decorator.rb",nil,repository_url
+  download_file "app/models/concerns/alchemy/content_dec.rb",nil,repository_url
+  download_file "app/models/concerns/alchemy/essence_html_dec.rb",nil,repository_url
+  download_file "app/models/concerns/alchemy/essence_richtext_dec.rb",nil,repository_url
+  download_file "app/models/concerns/alchemy/essence_text_dec.rb",nil,repository_url
+  download_file "app/models/concerns/alchemy/page_dec.rb",nil,repository_url
+  download_file "app/models/concerns/searchable.rb",nil,repository_url
+  download_file "app/views/alchemy/search/_form.html.erb",nil,repository_url
+  download_file "app/views/alchemy/search/_result.html.erb",nil,repository_url
+  download_file "app/views/alchemy/search/_results.html.erb",nil,repository_url
+  download_file "db/migrate/20180405200556_add_searchable_to_alchemy_essence_texts.alchemy_pg_search.rb",nil,repository_url
+  download_file "db/migrate/20180405200557_add_searchable_to_alchemy_essence_richtexts.alchemy_pg_search.rb",nil,repository_url
+  download_file "db/migrate/20180405200558_add_searchable_to_alchemy_essence_pictures.alchemy_pg_search.rb",nil,repository_url
+  download_file "config/initializers/archimedia_pgsearch.rb",nil,repository_url
+  download_file "config/initializers/pg_search.rb",nil,repository_url
   append_to_file "config/alchemy/elements.yml", <<-CODE
 - name: search_form
 hint: false
@@ -268,7 +211,7 @@ unique: true
   rails_command 'db:migrate'
 end
 
-def capistrano_finalize(deploy_with_docker=true)
+def capistrano_finalize(deploy_with_docker=true, repository_url="")
   run "bundle exec cap install"
 
   if deploy_with_docker
@@ -301,7 +244,7 @@ set :stackose_linked_folders, ['public/system',
       "docker-compose.yml",
       ".dockerignore"
     ].each do |f|
-      download_file f
+      download_file(f,nil,repository_url)
     end
 
     inject_into_file 'Capfile', :before => "# Load custom " do
@@ -323,7 +266,7 @@ require 'capistrano-db-tasks'\n\n"
 end
 
 
-def bootstrap_template
+def bootstrap_template(repository_url)
   # Configure generic template Bootstrap based
   append_to_file "config/alchemy/elements.yml", <<-CODE
 - name: single_image
@@ -364,27 +307,96 @@ def bootstrap_template
   elements: [header_landing,contenitore_colonne]      
             CODE
 
-  download_file "app/assets/stylesheets/_landing_page.scss"
-  download_file "app/lib/alchemy_bootstrap_grid/col_options_builder.rb"            
-  download_file "app/lib/alchemy_bootstrap_grid/row_options_builder.rb"            
-  download_file "app/views/alchemy/elements/_colonna_bootstrap_editor.html.erb"            
-  download_file "app/views/alchemy/elements/_colonna_bootstrap_view.html.erb"            
-  download_file "app/views/alchemy/elements/_contact_landing_form_view.html.erb"            
-  download_file "app/views/alchemy/elements/_contenitore_colonne_editor.html.erb"            
-  download_file "app/views/alchemy/elements/_contenitore_colonne_view.html.erb"            
-  download_file "app/views/alchemy/elements/_single_image_editor.html.erb"            
-  download_file "app/views/alchemy/elements/_single_image_view.html.erb"            
-  download_file "app/views/alchemy/elements/_single_text_editor.html.erb"            
-  download_file "app/views/alchemy/elements/_single_text_view.html.erb"            
-  download_file "app/views/alchemy/elements/_text_landing_view.html.erb"            
-  download_file "config/initializers/alchemy_bootstrap_grid.rb"            
-  download_file "config/initializers/recaptcha.rb" 
+  download_file "app/assets/stylesheets/_landing_page.scss",nil,repository_url
+  download_file "app/lib/alchemy_bootstrap_grid/col_options_builder.rb",nil,repository_url
+  download_file "app/lib/alchemy_bootstrap_grid/row_options_builder.rb",nil,repository_url
+  download_file "app/views/alchemy/elements/_colonna_bootstrap_editor.html.erb",nil,repository_url
+  download_file "app/views/alchemy/elements/_colonna_bootstrap_view.html.erb",nil,repository_url
+  download_file "app/views/alchemy/elements/_contact_landing_form_view.html.erb",nil,repository_url
+  download_file "app/views/alchemy/elements/_contenitore_colonne_editor.html.erb",nil,repository_url
+  download_file "app/views/alchemy/elements/_contenitore_colonne_view.html.erb",nil,repository_url
+  download_file "app/views/alchemy/elements/_single_image_editor.html.erb",nil,repository_url
+  download_file "app/views/alchemy/elements/_single_image_view.html.erb",nil,repository_url
+  download_file "app/views/alchemy/elements/_single_text_editor.html.erb",nil,repository_url
+  download_file "app/views/alchemy/elements/_single_text_view.html.erb",nil,repository_url
+  download_file "app/views/alchemy/elements/_text_landing_view.html.erb",nil,repository_url
+  download_file "config/initializers/alchemy_bootstrap_grid.rb",nil,repository_url
+  download_file "config/initializers/recaptcha.rb" ,nil,repository_url
 
   inject_into_file 'config/application.rb', after: "config.load_defaults 5.2" do
     "\nconfig.time_zone = 'Rome'"
     "\nconfig.i18n.default_locale = :it"
   end
 end
+
+
+
+
+
+version = %x(bin/rails version).gsub("\n", "").gsub("Rails", "")
+gem_version = Gem::Version.new(version)
+REPOSITORY_URL = "https://github.com/ArchimediaZerogroup/AlchemyBootstrapTemplate/raw/master"
+
+sass_requires = 'app/assets/stylesheets/sass_requires.scss'
+application_js = 'app/assets/javascripts/application.js'
+application_css = 'app/assets/stylesheets/application.css'
+pg_search = true
+
+
+ask("Remeber!!!! DISABLE_SPRING=true before command.")
+say "You are using Rails #{gem_version.inspect}"
+#if gem_version <= Gem::Version.new("5.2.4")
+  
+  default_gems(true)
+  default_patch(sass_requires, application_js, application_css)
+  bootstrap4(sass_requires,application_js)
+  fontawesome(application_css)
+  cookie_law(application_css, application_js)
+  recaptcha_gem
+  redis_backend
+
+  if pg_search
+    gem 'pg_search'
+  end
+
+  after_bundle do
+    rails_command 'alchemy:install'
+    generate 'friendly_id'
+    generate 'cookie_law:install'
+    rails_command 'alchemy_custom_model:install'
+    download_file "config/locales/devise.it.yml",nil,REPOSITORY_URL
+    download_file "config/puma.rb",nil,REPOSITORY_URL
+
+    if pg_search
+      pg_search_finalize(REPOSITORY_URL)
+    end
+
+    capistrano_finalize(true,REPOSITORY_URL)
+    bootstrap_template(REPOSITORY_URL)
+
+    download_file "app/helpers/link_languages_helper_decorator.rb",nil,REPOSITORY_URL
+    say "Created 'language_links_by_page' helper that must be insert into layouts (<%= language_links_by_page(@page)  %>)", [:red, :on_white, :bold]
+
+    generate 'alchemy_i18n:install --locales=it'
+
+    #Configure Alchemy defaults
+    append_to_file "config/alchemy/config.yml" do
+      "\nitems_per_page: 100"
+    end
+    #Cache assets initializer
+    download_file "config/initializers/static_assets_cache.rb",nil,REPOSITORY_URL
+    # Add devise css require
+    generate 'alchemy:devise:install'
+    rails_command "alchemy_custom_model:install"
+    
+    ["lib/tasks/alchemy_cache_clear.rake"].each do |f|
+      download_file f,nil,REPOSITORY_URL
+    end
+  end  
+
+#else
+#  raise "Alchemy it's not compatible with Rails version"
+#end
 
 
 
